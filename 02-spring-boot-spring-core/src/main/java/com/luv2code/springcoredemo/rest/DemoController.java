29caf202325c83146d283706543679ad1f90313d
aleksander <aleksander.lemiesz@protonmail.com>
2025-01-25 20:20:43 +0100

    05-qualifiers-and-primary

--- 02-spring-boot-spring-core/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-spring-core/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.luv2code.springcoredemo.rest;
 
 import com.luv2code.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +11,8 @@ public class DemoController {
 
     private Coach myCoach;
 
-//    @Autowired
-//    public DemoController(Coach theCoach) {
-//        myCoach = theCoach;
-//    }
-
     @Autowired
-    public void setCoach(Coach theCoach) {
+    public DemoController(Coach theCoach) {
         myCoach = theCoach;
     }
 
