c9cdad4583d04f7b4058a3f20403cf095cbf77f1
aleksander <aleksander.lemiesz@protonmail.com>
2025-01-25 20:41:10 +0100

    06-lazy-initialization

--- 02-spring-boot-spring-core/src/main/java/com/luv2code/springcoredemo/common/TennisCoach.java
+++ 02-spring-boot-spring-core/src/main/java/com/luv2code/springcoredemo/common/TennisCoach.java
@@ -1,9 +1,15 @@
 package com.luv2code.springcoredemo.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy
 public class TennisCoach implements Coach {
+
+    public TennisCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
 
     @Override
     public String getDailyWorkout() {
