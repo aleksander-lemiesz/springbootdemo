cc27921c6ac8ac81c1573119f618a4bee084e3cc
aleksander <aleksander.lemiesz@protonmail.com>
2025-01-25 20:55:14 +0100

    07-bean-scopes

--- 02-spring-boot-spring-core/src/main/java/com/luv2code/springcoredemo/common/TennisCoach.java
+++ 02-spring-boot-spring-core/src/main/java/com/luv2code/springcoredemo/common/TennisCoach.java
@@ -1,10 +1,8 @@
 package com.luv2code.springcoredemo.common;
 
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
-@Lazy
 public class TennisCoach implements Coach {
 
     public TennisCoach() {
